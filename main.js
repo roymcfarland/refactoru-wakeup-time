b919fa661eca01e0279789a4d1cc718cba080a74
Roy McFarland <roy.mcfarland@gmail.com>
2014-09-24 11:51:00 -0600

    added setInterval to make clock actually run

--- main.js
+++ main.js
@@ -3,24 +3,28 @@ $(document).on('ready', function() {
 
 
 /////////////////////////////////////
-//////////// VARIABLES //////////////
+/////////// FLUX CAPACITOR //////////
 /////////////////////////////////////
-
-	// Extracting the current time
 	
-var currentTime = function() {	
+var updateTime = function() {	
 	var date = new Date();
 	var hour = date.getHours();
-	// var convertedHour = function(h) {
-	// 	if(h > 12) {
-	// 		return h - 12;
-	// 	};
-	// };
-	// var realHour = convertedHour(hour);
 	var minutes = date.getMinutes();
-	// var realTime = realHour + ':' + minutes;
-	// var time = '<p class="time"></p>'
-	currentTime.text(hour + ':' + minutes);
+	
+	// Convert hours from military time
+	// to "normal" time
+	var convertedHour = function(h) {
+		if(h >= 13) {
+			return h - 12;
+		} else {
+			return h;
+		};
+	};
+	var realHour = convertedHour(hour);
+
+	// Combine converted hours and realtime minutes
+	// to the face of the clock
+	currentTime.text(realHour + ':' + minutes);
 	};
 
 
@@ -33,9 +37,9 @@ var currentTime = function() {
 	var innerShell = '<div class="innerShell"></div>';
 	var labels = '<div class="labels">PM<br><br><br><br><br>AUTO</div>';
 	var clockFace = '<div class="clockFace"></div>';
-	var currentTime = '<p class="time"></p>';
+	var currentTime = $('<div class="currentTime"></div>');
 	var amRadio = '<ul class="amRadio"><li>AM</li><li>53</li><li>60</li><li>70</li><li>90</li><li>110</li><li>140</li><li>170</li><span>x10</span><li>KHz</li></ul>';
-	var fmRadio = '<ul class="fmRadio"><li>FM</li><li>89</li><li>92</li><li>95</li><li>102</li><li>106</li><li>108</li><li>MHz</ul>';
+	var fmRadio = '<ul class="fmRadio"><li>FM</li><li>89</li><li>92</li><li>95</li><li>102</li><li>106</li><li>108</li><li>MHz</li></ul>';
 
 	$('.container').append(outerShell);
 	$('.outerShell').append(innerShell);
@@ -45,6 +49,6 @@ var currentTime = function() {
 	$('.innerShell').append(amRadio);
 	$('.innerShell').append(fmRadio);
 
-setInterval(currentTime, 1000);
-
+// Make the clock actually run in realtime
+setInterval(updateTime, 1000);
 });
